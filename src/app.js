00bf4d25fc21ce7aad2c5d33d289173ffd9a13bc
Jörg Wegener <hbpwbwpsuzib@joerg-wegener.de>
2024-04-29 11:18:06 +0200

    feat: Timeline-Styles korrigiert und Section zu Qualifikationen neu gestaltet (mit font awesome)

--- src/app.js
+++ src/app.js
@@ -4,18 +4,20 @@ import createServer from './createServer.js'
 
 const app = createServer()
 
-const server = app.listen('80', () => {
+const server = app.listen('8000', 'localhost', () => {
   console.log('Application started...')
 })
 
 process.on('SIGINT', () => {
   server.close(() => {
+    console.log('terminating...')
     process.exit(0)
   })
 })
 
 process.on('SIGTERM', () => {
   server.close(() => {
+    console.log('terminating...')
     process.exit(0)
   })
 })
