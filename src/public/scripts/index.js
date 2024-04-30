fdbb95c2a6822ccf6de2f1dc4d9ac180ad77f67c
Jörg Wegener <joergw@identage.com>
2024-04-30 06:16:21 +0200

    Cookie Consent Layer hinzugefügt (wip) #3

--- src/public/scripts/index.js
+++ src/public/scripts/index.js
@@ -1,10 +1,21 @@
+const LS_COOKIECONSENT_KEY = 'cookie-consent'
 const pageHeaderElement = document.getElementById('header')
 document.addEventListener('scroll', (event) => updateHeader(event))
 
-function updateHeader(event) {
+checkCookieConsent()
+
+function updateHeader() {
   if (window.scrollY > 200) {
     pageHeaderElement.classList.add('active')
   } else {
     pageHeaderElement.classList.remove('active')
+  }
+}
+
+function checkCookieConsent() {
+  let consent = localStorage.getItem(LS_COOKIECONSENT_KEY)
+  if (!consent) {
+    var cookieConsentElement = document.getElementById('cookie-consent')
+    cookieConsentElement.classList.add('active')
   }
 }
