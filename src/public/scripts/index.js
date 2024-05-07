bd67df52114585e208aec83a02770a52df7403d7
Jörg Wegener <joergw@identage.com>
2024-05-07 08:40:38 +0200

    Text im Cookie-Consent formatiert und testweise einfache Logik zum Ausblenden hinzugefügt

--- src/public/scripts/index.js
+++ src/public/scripts/index.js
@@ -18,4 +18,23 @@ function checkCookieConsent() {
     var cookieConsentElement = document.getElementById('cookie-consent')
     cookieConsentElement.classList.add('active')
   }
+
+  var acceptNecessaryCookiesButton = document.getElementById(
+    'acceptNecessaryCookies',
+  )
+  acceptNecessaryCookiesButton.addEventListener('click', acceptNecessaryCookies)
+  var accecptAllCookiesButton = document.getElementById('acceptAllCookies')
+  accecptAllCookiesButton.addEventListener('click', accecptAllCookies)
+}
+
+function acceptNecessaryCookies() {
+  console.log('necessary cookies accepted')
+  var cookieConsentElement = document.getElementById('cookie-consent')
+  cookieConsentElement.classList.remove('active')
+}
+
+function accecptAllCookies() {
+  console.log('all cookies accepted')
+  var cookieConsentElement = document.getElementById('cookie-consent')
+  cookieConsentElement.classList.remove('active')
 }
